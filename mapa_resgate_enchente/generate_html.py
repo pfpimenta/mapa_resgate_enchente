a04cd509f26c7f9cb5b670d07cea953baaa33a73
pfpimenta <pedrofolettopimenta@gmail.com>
2024-05-09 15:15:22 -0300

    fixing mistakes

--- mapa_resgate_enchente/generate_html.py
+++ mapa_resgate_enchente/generate_html.py
@@ -4,14 +4,20 @@ from folium.plugins import MarkerCluster
 import shutil
 from datetime import datetime
 import os
-from paths import HTML_BACKUPS_FOLDERPATH, HTMLMAPA_FILEPATH, HTMLINDEX_FILEPATH
+from paths import (
+    HTML_BACKUPS_FOLDERPATH,
+    HTMLMAPA_FILEPATH,
+    HTMLINDEX_FILEPATH,
+    HTMLMAPA_24h_FILEPATH,
+    HTMLMAPA_HOJE_FILEPATH,
+)
 
 
 def save_backup_html(has_map_data_changed: bool = True):
     # save backup only if the map data has changed
     os.makedirs(HTML_BACKUPS_FOLDERPATH, exist_ok=True)
     if has_map_data_changed:
-        now = datetime.now() # current date and time
+        now = datetime.now()  # current date and time
         format = "%Y_%m_%d-%H_%M_%S"
         timestamp = now.strftime(format)
         backup_html_filepath = HTML_BACKUPS_FOLDERPATH / f"backup_mapa_{timestamp}.html"
@@ -19,14 +25,27 @@ def save_backup_html(has_map_data_changed: bool = True):
         print(f"Saved backup {backup_html_filepath}")
     else:
         backup_filepath_list = os.listdir(HTML_BACKUPS_FOLDERPATH)
-        backup_filepath_list = list(filter(lambda x: x[-5:] == '.html', backup_filepath_list))
-        last_backup_filepath = HTML_BACKUPS_FOLDERPATH / sorted(backup_filepath_list)[-1]
+        backup_filepath_list = list(
+            filter(lambda x: x[-5:] == ".html", backup_filepath_list)
+        )
+        last_backup_filepath = (
+            HTML_BACKUPS_FOLDERPATH / sorted(backup_filepath_list)[-1]
+        )
         print(f"Nothing changed since last backup {last_backup_filepath}")
 
 
-def generate_html(df_mapped: pd.DataFrame, has_map_data_changed: bool = True):
-    """ gera mapa a partir do DataFrame df_mapped
+def generate_html_filtered(df_mapped: pd.DataFrame) -> None:
+    """gera e salva dois mapas HTML:
+    - mapa_24h.html
+        contem somente os pedidos registrados nas ultimas 24h
+    - mapa_hoje.html
+        contem somente os pedidos registrados no dia de hoje
     """
+    pass  # TODO
+
+
+def generate_html(df_mapped: pd.DataFrame, has_map_data_changed: bool = True):
+    """gera mapa a partir do DataFrame df_mapped"""
     # Create a map centered around Porto Alegre
     map_porto_alegre = folium.Map(location=[-30.0346, -51.2177], zoom_start=12)
 
@@ -57,23 +76,22 @@ def generate_html(df_mapped: pd.DataFrame, has_map_data_changed: bool = True):
         Número: {num} <br>
 
         Complemento: {compl}<br>
-        """.format(data = row["DATAHORA"],
-                cidade = row["CIDADE"],
-                desc = row['DESCRICAORESGATE'],
-                det = row["DETALHE"],
-                info = row['INFORMACOES'],
-                contato = row["CONTATORESGATADO"],
-                logradouro = row["LOGRADOURO"],
-                num = row["NUM"],
-                compl = row["COMPL"]
-                )
+        """.format(
+            data=row["DATAHORA"],
+            cidade=row["CIDADE"],
+            desc=row["DESCRICAORESGATE"],
+            det=row["DETALHE"],
+            info=row["INFORMACOES"],
+            contato=row["CONTATORESGATADO"],
+            logradouro=row["LOGRADOURO"],
+            num=row["NUM"],
+            compl=row["COMPL"],
+        )
         lat = row["latitude"]
         long = row["longitude"]
         iframe = folium.IFrame(html)
-        popup = folium.Popup(iframe,
-                            min_width=500,
-                            max_width=500)
-        folium.Marker([lat,long], popup=popup).add_to(marker_cluster)
+        popup = folium.Popup(iframe, min_width=500, max_width=500)
+        folium.Marker([lat, long], popup=popup).add_to(marker_cluster)
 
     # save HTML
     map_porto_alegre.save(HTMLMAPA_FILEPATH)
--- mapa_resgate_enchente/generate_map_data.py
+++ mapa_resgate_enchente/generate_map_data.py
@@ -1,4 +1,4 @@
-#mapa resgate script 2024_06_05_0
+# mapa resgate script 2024_06_05_0
 from difflib import SequenceMatcher
 import pandas as pd
 import requests
@@ -10,109 +10,132 @@ from io import BytesIO
 from datetime import datetime
 import geopy
 from typing import Tuple
-from generate_html import generate_html
-from paths import DF_SHEETS_FILEPATH, DF_GABINETE_FILEPATH, DF_WITHOUT_COORDS_FILEPATH, DF_UNMAPPED_FILEPATH, DF_MAPPED_FILEPATH, MAPPED_BACKUPS_FOLDERPATH
+from paths import (
+    DF_SHEETS_FILEPATH,
+    DF_GABINETE_FILEPATH,
+    DF_WITHOUT_COORDS_FILEPATH,
+    DF_UNMAPPED_FILEPATH,
+    DF_MAPPED_FILEPATH,
+    MAPPED_BACKUPS_FOLDERPATH,
+)
 from dotenv import load_dotenv
 
 GEOLOCATOR = Photon(user_agent="measurements")
 
 # parameters
-URL_DADOS_GABINETE = 'https://onedrive.live.com/download?resid=C734B4D1CCD6CEA6!94437&authkey=!ABnn6msPt2x5OFk'
-IDENTIFIER_COLUMNS = ["DATAHORA", "NUMPESSOAS", "DETALHES", "LOGRADOURO", "CONTATORESGATADO", 
-                      "DESCRICAORESGATE", "NUM","COMPLEMENTO","BAIRRO","CIDADE"]
+URL_DADOS_GABINETE = "https://onedrive.live.com/download?resid=C734B4D1CCD6CEA6!94437&authkey=!ABnn6msPt2x5OFk"
+IDENTIFIER_COLUMNS = [
+    "DATAHORA",
+    "NUMPESSOAS",
+    "DETALHES",
+    "LOGRADOURO",
+    "CONTATORESGATADO",
+    "DESCRICAORESGATE",
+    "NUM",
+    "COMPLEMENTO",
+    "BAIRRO",
+    "CIDADE",
+]
 
 # Access the api key
-load_dotenv("api_key.env")
-api_key = os.getenv('API_KEY')
+# load_dotenv("api_key.env")
+api_key = os.getenv("API_KEY", None)
+if api_key is None:
+    raise ValueError("API_KEY not set! Please set this env var.")
+
 
 def get_place_id(input_text: str, api_key: str) -> str:
     endpoint_url = "https://maps.googleapis.com/maps/api/place/autocomplete/json"
-    params = {
-        'input': input_text,
-        'key': api_key
-    }
+    params = {"input": input_text, "key": api_key}
     try:
         response = requests.get(endpoint_url, params=params)
-        place_id = response.json()['predictions'][0]['place_id']
+        place_id = response.json()["predictions"][0]["place_id"]
         return place_id
     except:
+        print(response.status_code)
+        print(response.text)
+        print(api_key)
         return False
+
 
 def get_location(place_id: str, api_key: str) -> Tuple[float, float]:
     details_url = "https://maps.googleapis.com/maps/api/place/details/json"
-    params = {
-        'place_id': place_id,
-        'fields': 'geometry',
-        'key': api_key
-    }
+    params = {"place_id": place_id, "fields": "geometry", "key": api_key}
     try:
         response = requests.get(details_url, params=params)
-        location = response.json()['result']['geometry']['location']
-        return location['lat'], location['lng']
+        location = response.json()["result"]["geometry"]["location"]
+        return location["lat"], location["lng"]
     except:
         return False, False
 
+
 def similar(a: str, b: str):
     return SequenceMatcher(None, a, b).ratio()
+
 
 def similarity_value(x: pd.core.series.Series, location: geopy.location.Location):
     if "city" in location.raw["properties"].keys():
         city = location.raw["properties"]["city"]
     else:
         return False
-        
+
     if "street" in location.raw["properties"].keys():
         street = location.raw["properties"]["street"]
     elif "name" in location.raw["properties"].keys():
         street = location.raw["properties"]["name"]
     else:
         return False
-    
+
     sims = [similar(street, x["LOGRADOURO"]), similar(city, x["CIDADE"])]
     min_sim = min(sims)
     return min_sim
 
+
 def get_coords(row: pd.core.series.Series):
-    address = row['address']
+    address = row["address"]
     place_id = get_place_id(address, api_key)
     if place_id:
         latitude, longitude = get_location(place_id, api_key)
         if latitude and longitude:
-            return [latitude, longitude, "1"] # Attempt to extract the ZIP code
+            return [latitude, longitude, "1"]  # Attempt to extract the ZIP code
         else:
             print(f"Failed to find a place like: {address}")
-            return ["","","0"]            
+            return ["", "", "0"]
     else:
         print(f"Failed to fetch the coordinates for: {address}")
-        return ["","","0"]
-    
+        return ["", "", "0"]
+
+
 def get_coords_df(df_sheets: pd.DataFrame):
     print(f"Getting coordinates for {len(df_sheets)} addresses...")
     df = df_sheets.copy()
-    df["address"] = df["LOGRADOURO"] + "," + df["NUM"] + ", " + df["BAIRRO"]  + ", " + df["CIDADE"]
+    df["address"] = (
+        df["LOGRADOURO"] + "," + df["NUM"] + ", " + df["BAIRRO"] + ", " + df["CIDADE"]
+    )
     outs = []
     L = len(df)
     idx = 0
     for index, row in df.iterrows():
-        if index % 5==0:
-            print("row {}/{}".format(idx,L)) #print current step
+        if index % 5 == 0:
+            print("row {}/{}".format(idx, L))  # print current step
         out = get_coords(row)
         outs.append(out)
         idx += 1
-        
+
     lats = [str(o[0]) for o in outs]
-    longs =[str(o[1]) for o in outs]
+    longs = [str(o[1]) for o in outs]
     sucs = [str(o[2]) for o in outs]
     df["latitude"] = lats
     df["longitude"] = longs
     df["success"] = sucs
-    df = df[df["success"]=="1"]
+    df = df[df["success"] == "1"]
     return df
-        
 
-#----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
 # Pull data from sheets -----------------------------------------------------
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
 
 def get_google_sheet(spreadsheet_id: str) -> pd.DataFrame:
     url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=csv"
@@ -122,45 +145,66 @@ def get_google_sheet(spreadsheet_id: str) -> pd.DataFrame:
         df = pd.read_csv(csv_data, sep=",", dtype=str)
         print(f"Fetched {len(df)} rows from LAGON Google Sheets")
     else:
-        print(f"Requisicao dos dados do Google Sheet falhou com erro {response.status_code}")
+        print(
+            f"Requisicao dos dados do Google Sheet falhou com erro {response.status_code}"
+        )
         sys.exit(1)
     return df
 
+
 def prepare_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     cols = df.iloc[0]
-    #renomear colunas para evitar incompatibilidades com o sheet
-    cols[0:14] = ['DATAHORA','NUMPESSOAS','DETALHES','LOGRADOURO','CONTATORESGATADO','DESCRICAORESGATE',
-              'NUM','COMPLEMENTO','BAIRRO','CIDADE','CEP','NOMEPESSOAS','CADASTRADO','ENCERRADO']
-    named_cols = [c for c in cols if len(c)>0]
+    # renomear colunas para evitar incompatibilidades com o sheet
+    cols[0:14] = [
+        "DATAHORA",
+        "NUMPESSOAS",
+        "DETALHES",
+        "LOGRADOURO",
+        "CONTATORESGATADO",
+        "DESCRICAORESGATE",
+        "NUM",
+        "COMPLEMENTO",
+        "BAIRRO",
+        "CIDADE",
+        "CEP",
+        "NOMEPESSOAS",
+        "CADASTRADO",
+        "ENCERRADO",
+    ]
+    named_cols = [c for c in cols if len(c) > 0]
     df.columns = cols
     df = df[named_cols]
     df = df.iloc[1:]
     return df
 
+
 def get_df_sheets() -> pd.DataFrame:
     # get data from google sheets
-    df_sheets = get_google_sheet(spreadsheet_id="1JD5serjAxnmqJWP8Y51A6wEZwqZ9A7kEUH1ZwGBx1tY")
-    df_sheets = prepare_dataframe(df_sheets) 
-    df_sheets = df_sheets[df_sheets['LOGRADOURO'].notna()]
-    df_sheets["len"] = df_sheets["LOGRADOURO"].apply(lambda x : len(x))
-    df_sheets = df_sheets[df_sheets["len"]>0]
-    df_sheets = df_sheets[df_sheets["ENCERRADO"]!="S"]
-    df_sheets = df_sheets.drop("len",axis = 1)
+    df_sheets = get_google_sheet(
+        spreadsheet_id="1JD5serjAxnmqJWP8Y51A6wEZwqZ9A7kEUH1ZwGBx1tY"
+    )
+    df_sheets = prepare_dataframe(df_sheets)
+    df_sheets = df_sheets[df_sheets["LOGRADOURO"].notna()]
+    df_sheets["len"] = df_sheets["LOGRADOURO"].apply(lambda x: len(x))
+    df_sheets = df_sheets[df_sheets["len"] > 0]
+    df_sheets = df_sheets[df_sheets["ENCERRADO"] != "S"]
+    df_sheets = df_sheets.drop("len", axis=1)
     print(f"Fetched {len(df_sheets)} rows from LAGON")
     return df_sheets
+
 
 def get_df_gabinete() -> pd.DataFrame:
     response = requests.get(URL_DADOS_GABINETE)
     assert response.status_code == 200, "Erro ao baixar o arquivo"
     # Usando pandas para ler os dados da planilha
     df_gabinete = pd.read_excel(BytesIO(response.content))
-    df_gabinete = df_gabinete[df_gabinete["ENCERRADO"]!="S"]
+    df_gabinete = df_gabinete[df_gabinete["ENCERRADO"] != "S"]
     print(f"Fetched {len(df_gabinete)} rows from GABINETE")
     return df_gabinete
 
+
 def process_df_gabinete(df_gabinete: pd.DataFrame) -> pd.DataFrame:
-    """ deixa as colunas iguais a df_sheets
-    """
+    """deixa as colunas iguais a df_sheets"""
     # df_sheets.columns:
     # ['DATAHORA', 'DESCRICAORESGATE', 'DETALHE', 'LOGRADOURO',
     # 'CONTATORESGATADO', 'INFORMACOES', 'NUM', 'COMPL', 'BAIRRO', 'CIDADE',
@@ -168,10 +212,14 @@ def process_df_gabinete(df_gabinete: pd.DataFrame) -> pd.DataFrame:
     # df_gabinete.columns:
     # ['Unnamed: 0', 'PRIORIDADES', 'Bairro', 'OBSERVAÇÃO', 'CONTATO', 'OBS',
     # 'RESGATADOS ', 'Unnamed: 7'],
-    df_gabinete.rename(columns={"Bairro": "BAIRRO",
-                                "OBSERVAÇÃO": "DESCRICAORESGATE",
-                                "CONTATO": "CONTATORESGATADO"
-                                }, inplace=True)
+    df_gabinete.rename(
+        columns={
+            "Bairro": "BAIRRO",
+            "OBSERVAÇÃO": "DESCRICAORESGATE",
+            "CONTATO": "CONTATORESGATADO",
+        },
+        inplace=True,
+    )
     df_gabinete["ADDRESS"] = df_gabinete.iloc[:, 0] + " " + df_gabinete["PRIORIDADES"]
     # ADDRESS = LOGRADOURO + NUMERO + TUDO
     df_gabinete["LOGRADOURO"] = df_gabinete["ADDRESS"]
@@ -180,90 +228,130 @@ def process_df_gabinete(df_gabinete: pd.DataFrame) -> pd.DataFrame:
     df_gabinete["CIDADE"] = ""
     df_gabinete["DETALHE"] = ""
     df_gabinete["CEP"] = ""
-    df_gabinete["INFORMACOES"] = ""
+    df_gabinete["INFORMACOES"] = df_gabinete["NUM_PESSOAS"]
     df_gabinete["NOMEPESSOAS"] = ""
     df_gabinete["CADASTRADO"] = ""
-    df_gabinete.drop(axis=1, labels=['Unnamed: 0', 'Unnamed: 7', 'PRIORIDADES'], inplace=True)
+    df_gabinete.drop(
+        axis=1, labels=["Unnamed: 0", "Unnamed: 7", "PRIORIDADES"], inplace=True
+    )
     return df_gabinete
 
-def get_df_unmapped(df_previous: pd.DataFrame, df_without_coords : pd.DataFrame) -> pd.DataFrame:
-    df_unmapped = pd.merge(df_without_coords, df_previous[IDENTIFIER_COLUMNS+["success","latitude","longitude"]], on = IDENTIFIER_COLUMNS, how = "left")
-    df_unmapped = df_unmapped[df_unmapped["success"]!="1"]
+
+def get_df_unmapped(
+    df_previous: pd.DataFrame, df_without_coords: pd.DataFrame
+) -> pd.DataFrame:
+    df_unmapped = pd.merge(
+        df_without_coords,
+        df_previous[IDENTIFIER_COLUMNS + ["success", "latitude", "longitude"]],
+        on=IDENTIFIER_COLUMNS,
+        how="left",
+    )
+    df_unmapped = df_unmapped[df_unmapped["success"] != "1"]
     df_unmapped = df_unmapped[list(df_without_coords.columns)]
     return df_unmapped
 
+
 def get_df_with_coordinates(df_without_coords: pd.DataFrame) -> pd.DataFrame:
+
+    # remove ENCERRADOS
+    print("Removing ENCERRADO")
+    print("Before removal: {} rows".format(len(df_mapped)))
+    df_without_coords = df_without_coords[df_without_coords["ENCERRADO"] != "S"]
+    df_without_coords = df_without_coords[df_without_coords["ENCERRADO"] != "s"]
+    print("After removal: {} rows".format(len(df_without_coords)))
 
     if not os.path.exists(DF_MAPPED_FILEPATH):
         df_mapped = get_coords_df(df_without_coords)
-        df_mapped.to_csv(DF_MAPPED_FILEPATH, index = False)
+        df_mapped.to_csv(DF_MAPPED_FILEPATH, index=False)
         print(f"Saved {DF_MAPPED_FILEPATH}")
-        #collect info from unmapped (required to save unmapped values)
-        df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype = str)
+        # collect info from unmapped (required to save unmapped values)
+        df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype=str)
         df_unmapped = get_df_unmapped(df_previous, df_without_coords)
     else:
-        df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype = str)
+        df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype=str)
         len0 = len(df_previous)
         df_unmapped = get_df_unmapped(df_previous, df_without_coords)
-        df_mapped = get_coords_df(df_unmapped) # DEBUG
-        df_mapped = pd.concat([df_mapped,df_previous])
-        
+        df_mapped = get_coords_df(df_unmapped)  # DEBUG
+        df_mapped = pd.concat([df_mapped, df_previous])
+
         # save DataFrame with coordinates locally
-        if len(df_mapped)>len0:
+        if len(df_mapped) > len0:
             df_mapped = df_mapped.drop_duplicates(IDENTIFIER_COLUMNS)
-            df_mapped.to_csv(DF_MAPPED_FILEPATH, index = False)
-            print(f"Saved {DF_MAPPED_FILEPATH}")
 
-    num_mapped = len(df_mapped)
-    num_unmapped = len(df_unmapped)
-    print(f"num_mapped: {num_mapped}, num_unmapped: {num_unmapped}")
 
-    #update unmapped rows
-    df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype = str)
-    df_unmapped = pd.merge(df_without_coords, df_previous[IDENTIFIER_COLUMNS + ["success","latitude","longitude"]], on = IDENTIFIER_COLUMNS, how = "left")
-    df_unmapped = df_unmapped[df_unmapped["success"]!="1"]
+    # update unmapped rows
+    df_previous = pd.read_csv(DF_MAPPED_FILEPATH, dtype=str)
+    df_unmapped = pd.merge(
+        df_without_coords,
+        df_previous[IDENTIFIER_COLUMNS + ["success", "latitude", "longitude"]],
+        on=IDENTIFIER_COLUMNS,
+        how="left",
+    )
+    df_unmapped = df_unmapped[df_unmapped["success"] != "1"]
     df_unmapped = df_unmapped[list(df_without_coords.columns)]
-    df_unmapped.to_csv(DF_UNMAPPED_FILEPATH)
-    print(f"Saved {DF_UNMAPPED_FILEPATH}")
 
     # treat NaN values
     df_mapped = df_mapped[df_mapped["latitude"].notna()]
 
-    #remove ENCERRADOS
-    print("Removing ENCERRADO")
-    print("Before removal: {} rows".format(len(df_mapped)))
-    df_mapped = pd.merge(df_without_coords, df_mapped[IDENTIFIER_COLUMNS + ["success","latitude","longitude"]], 
-                  on = IDENTIFIER_COLUMNS, how = "left")
-    df_mapped = df_mapped[df_mapped["success"]=="1"]
-    df_mapped = df_mapped[df_mapped["ENCERRADO"]!="S"]
-    print("After removal: {} rows".format(len(df_mapped)))
+    # ?
+    df_mapped = pd.merge(
+        df_without_coords,
+        df_mapped[IDENTIFIER_COLUMNS + ["success", "latitude", "longitude"]],
+        on=IDENTIFIER_COLUMNS,
+        how="left",
+    )
+    df_mapped = df_mapped[df_mapped["success"] == "1"]
+
+    # df_mapped 
+    num_mapped = len(df_mapped)
+    num_unmapped = len(df_unmapped)
+    print(f"num_mapped: {num_mapped}, num_unmapped: {num_unmapped}")
+
+    # save df_mapped e df_unmapped
+    df_mapped.to_csv(DF_MAPPED_FILEPATH, index=False)
+    print(f"Saved {DF_MAPPED_FILEPATH}")
+    df_unmapped.to_csv(DF_UNMAPPED_FILEPATH)
+    print(f"Saved {DF_UNMAPPED_FILEPATH}")
 
     return df_mapped, df_unmapped
+
 
 def save_backups(df_mapped: pd.DataFrame) -> bool:
     # check if map data has changed since last update
     os.makedirs(MAPPED_BACKUPS_FOLDERPATH, exist_ok=True)
     backup_filepath_list = os.listdir(MAPPED_BACKUPS_FOLDERPATH)
-    backup_filepath_list = list(filter(lambda x: x[-4:] == '.csv', backup_filepath_list))
+    backup_filepath_list = list(
+        filter(lambda x: x[-4:] == ".csv", backup_filepath_list)
+    )
     if len(backup_filepath_list) > 0:
-        last_backup_filepath = MAPPED_BACKUPS_FOLDERPATH / sorted(backup_filepath_list)[-1]
-        last_df_mapped_backup = pd.read_csv(last_backup_filepath, dtype = str)
-        has_map_data_changed = not df_mapped.fillna('').reset_index(drop=True).eq(last_df_mapped_backup.fillna('').reset_index(drop=True)).all().all()
+        last_backup_filepath = (
+            MAPPED_BACKUPS_FOLDERPATH / sorted(backup_filepath_list)[-1]
+        )
+        last_df_mapped_backup = pd.read_csv(last_backup_filepath, dtype=str)
+        has_map_data_changed = (
+            not df_mapped.fillna("")
+            .reset_index(drop=True)
+            .eq(last_df_mapped_backup.fillna("").reset_index(drop=True))
+            .all()
+            .all()
+        )
     else:
         last_backup_filepath = " "
         has_map_data_changed = True
 
-    # save df_mapped backup if map data has changed 
+    # save df_mapped backup if map data has changed
     if has_map_data_changed:
-        now = datetime.now() # current date and time
+        now = datetime.now()  # current date and time
         format = "%Y_%m_%d-%H_%M_%S"
         timestamp = now.strftime(format)
-        backup_csv_filepath = MAPPED_BACKUPS_FOLDERPATH / f"backup_df_mapped_{timestamp}.csv"
+        backup_csv_filepath = (
+            MAPPED_BACKUPS_FOLDERPATH / f"backup_df_mapped_{timestamp}.csv"
+        )
         df_mapped.to_csv(path_or_buf=backup_csv_filepath, index=False)
         print(f"Saved {backup_csv_filepath}")
     else:
         print(f"Nothing changed since last backup: {last_backup_filepath}")
-    
+
     return has_map_data_changed
 
 
@@ -292,7 +380,7 @@ def generate_map_data(debug: bool) -> Tuple[pd.DataFrame, bool]:
     # format df_gabinete to have the same columns as df_sheets
     df_gabinete = process_df_gabinete(df_gabinete)
 
-    # save CSVs 
+    # save CSVs
     df_sheets.to_csv(path_or_buf=DF_SHEETS_FILEPATH)
     print(f"Saved {DF_SHEETS_FILEPATH}")
     df_gabinete.to_csv(path_or_buf=DF_GABINETE_FILEPATH)
@@ -310,10 +398,13 @@ def generate_map_data(debug: bool) -> Tuple[pd.DataFrame, bool]:
         df_without_coords = df_without_coords.iloc[0:50]
 
     # pegar coordenadas
-    df_mapped, df_unmapped = get_df_with_coordinates(df_without_coords=df_without_coords)
+    df_mapped, df_unmapped = get_df_with_coordinates(
+        df_without_coords=df_without_coords
+    )
 
-    # TODO organizar numa funcao:
+    # salva df_mapped e 
+
+    # salva backup do df_mapped caso tenha mudado desde o ultimo backup
     has_map_data_changed = save_backups(df_mapped=df_mapped)
 
     return df_mapped, has_map_data_changed
-
